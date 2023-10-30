2a840fb25dc1e60be9bc14e661a522ae3e0270a3
LVRV11 <lavrovdmitrii@gmail.com>
2023-10-30 21:39:59 +0100

    11

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,22 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const Center(
-      child: Text(
-        "Hello Flutter",
-        textDirection: TextDirection.ltr,
-        style: TextStyle(
-            fontWeight: FontWeight.bold, fontSize: 30, color: Colors.red),
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 45, 45, 44),
+        appBar: AppBar(
+          title: const Text("My wiggyziggy App"),
+          centerTitle: true,
+        ),
+        body: const Center(
+          child: Text(
+            "This is my Homepage",
+            style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 240, 244, 176)),
+          ),
+        ),
       ),
     ),
   );
