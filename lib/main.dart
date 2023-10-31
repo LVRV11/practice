85d65f93c92fa3fb873596d7c7d215c5c0b4b864
LVRV11 <lavrovdmitrii@gmail.com>
2023-10-31 22:37:28 +0100

    32

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,89 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyFirstApp());
+  runApp(const MyApp());
 }
 
-class MyFirstApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late bool _loading;
+  late double _progressValue;
+
+  @override
+  void initState() {
+    _loading = false;
+    _progressValue = 0.0;
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: const Color.fromARGB(255, 78, 78, 78),
+        backgroundColor: Colors.indigo,
         appBar: AppBar(
-          title: const Text("My First App"),
+          title: const Text('My First App'),
           centerTitle: true,
         ),
         body: Center(
           child: Container(
             padding: const EdgeInsets.all(16),
-            child: const Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  LinearProgressIndicator(value: 23),
-                  Text(
-                    '23%',
-                    style: TextStyle(color: Colors.white, fontSize: 20),
+            child: _loading
+                ? Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      LinearProgressIndicator(value: _progressValue),
+                      Text(
+                        '${(_progressValue * 100).round()}%',
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  )
+                : const Text(
+                    'Text button to download',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
                   ),
-                  Text(
-                    'Press Button',
-                    style: TextStyle(color: Colors.white, fontSize: 20),
-                  ),
-                ]),
           ),
         ),
-        floatingActionButton: const FloatingActionButton(
-          onPressed: null,
-          child: Icon(Icons.cloud_download),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              _loading = !_loading;
+              _updateProgress();
+            });
+          },
+          child: const Icon(Icons.cloud_download),
         ),
       ),
     );
+  }
+
+  void _updateProgress() {
+    const oneSec = Duration(seconds: 1);
+    Timer.periodic(oneSec, (Timer t) {
+      setState(() {
+        _progressValue += 0.2;
+        if (_progressValue.toStringAsFixed(1) == '1.0') {
+          _loading = false;
+          t.cancel();
+          _progressValue = 0.0;
+          return;
+        }
+      });
+    });
   }
 }
